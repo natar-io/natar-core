6f0ba5184cd10cf3910837e40910f8e8bd37c6c0
Jeremy Laviole <poqudrof@gmail.com>
2019-02-08 11:30:40 +0100

    ignore deps folder Added Test app.

---
+++ src/main/java/tech/lity/rea/nectar/camera/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tech.lity.rea.nectar.camera;
+
+import processing.core.PVector;
+import toxi.geom.Vec3D;
+
+/**
+ *
+ * @author ditrop
+ */
+public class Test {
+    public static void main(String[] args){
+        Vec3D v = new Vec3D();
+        PVector v2 = new PVector();
+        System.out.println("Test is OK!" + v.toString() + " " + v2.toString());
+    }
+    
+}
